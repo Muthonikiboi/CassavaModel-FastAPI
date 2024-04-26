ed85d14a5928f90dbc06ed0d6acef32bb9fdcf1d
Muthhonikiboi <kiboijoye254@gmail.com>
2024-04-27 00:22:28 +0300

    ADD:File Path Update

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app.add_middleware(
 )
 
 # Path to the saved model directory
-saved_model_dir = '/home/zakidev/Desktop/FastAPI/Models/8'
+saved_model_dir = 'Models/8'
 
 # Load the model using TFSMLayer
 model_layer = tf.keras.layers.TFSMLayer(saved_model_dir, call_endpoint='serving_default')
